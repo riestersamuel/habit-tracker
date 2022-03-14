8cc3784262dab95a6b4a61fc9bd2f645aa50606e
Victor Merkulov <merkulov@hdm-stuttgart.de>
2022-03-14 15:49:30 +0100

    Use Java 17 and cleanup
    - Use Junit 5
    - Simplify .gitignore
    - Remove .idea files
    - Configure JavaFX to also run in IDEA

--- src/main/java/mainpackage/Main.java
+++ src/main/java/mainpackage/Main.java
@@ -7,13 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         URL fxmlFileUrl = getClass().getClassLoader().getResource("sample.fxml");
-        Parent root = FXMLLoader.load(fxmlFileUrl);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(fxmlFileUrl));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
