a3c318cd627b0988e60404f96944db395e64aeb5
riestersamuel <riestersamuel@icloud.com>
2022-04-21 16:20:24 +0200

    Changes by steffen

--- src/main/java/mainpackage/Controller.java
+++ src/main/java/mainpackage/Controller.java
@@ -6,11 +6,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 
+import java.util.ArrayList;
+
 public class Controller {
     @FXML
     ListView lvJournal;
     @FXML
-    TextArea taNewJournal;
+    TextArea taNewJournal; // Hier auslesen
     @FXML
     Label lWelcome;
     @FXML
@@ -19,4 +21,20 @@ public class Controller {
     Button btnRemoveHabit;
     @FXML
     Label lMiniJournal;
+    @FXML
+    Button btnAddJournal;
+
+    @FXML
+    private void addNewEntry() {
+        ArrayList<Habit> al = new ArrayList<>();
+        al.add(new Habit("Könken", 100, 7));
+        al.add(new Habit("Saufen", 100, 7));
+        al.add(new Habit("Orgeln", 100, 7));
+        al.add(new Habit("Julius duschen", 100, 7));
+
+        System.out.println(al.get(0).getName());
+
+        lvJournal.getItems().add(0, taNewJournal.getText());
+        taNewJournal.clear();
+    }
 }
