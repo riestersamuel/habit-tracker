b7baaa0adb31e4b648860f52de33468a4daca3fd
Steffen <info.stapps@gmail.com>
2022-05-14 23:19:28 +0200

    corrected typo

--- src/main/java/com/teampingui/controllers/ChallengeController.java
+++ src/main/java/com/teampingui/controllers/ChallengeController.java
@@ -5,7 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class ChallangeController {
+public class ChallengeController {
     @FXML
     Button btnHabits, btnChallenge, btnSettings;
 
