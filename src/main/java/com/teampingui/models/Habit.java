0b716102d9d81b95edb0e828e0b11fce78f671f5
tb173 <tb173@hdm-stuttgart.de>
2022-05-15 16:13:17 +0200

    Tableview: removed "bug" of multiple selections + Added "Reps"

--- src/main/java/com/teampingui/models/Habit.java
+++ src/main/java/com/teampingui/models/Habit.java
@@ -6,13 +6,37 @@ public class Habit {
 
     private final StringProperty name = new SimpleStringProperty();
 
-    private final BooleanProperty done = new SimpleBooleanProperty();
+    private final BooleanProperty checked = new SimpleBooleanProperty();
 
     private final IntegerProperty reps = new SimpleIntegerProperty();  
 
-    public Habit(String name, boolean done, Integer reps) {
+    public Habit(String name, boolean checked, int reps) {
         this.name.set(name);
-        this.done.set(done);
+        this.checked.set(checked);
+        this.reps.set(reps);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public boolean isChecked() {
+        return checked.get();
+    }
+
+    public int getReps() {
+        return reps.get();
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked.set(checked);
+    }
+
+    public void setReps(int reps) {
         this.reps.set(reps);
     }
 
@@ -21,7 +45,9 @@ public class Habit {
     }
 
     public  BooleanProperty doneProperty() {
-        return done;
+        return checked;
     }
+
+    public IntegerProperty repsProperty() {return reps;}
 
 }
