61c61a134c3446d65b6d8138a33084e09c397f4e
riestersamuel <riestersamuel@icloud.com>
2022-06-18 13:00:21 +0200

    Added first (uncomplete) test

--- src/test/java/mainpackage/ExampleTest.java
+++ src/test/java/mainpackage/ExampleTest.java
@@ -1,13 +1,36 @@
 package mainpackage;
 
+import com.teampingui.dao.Database;
+import com.teampingui.dao.JournalDAO;
+import com.teampingui.exceptions.JournalDaoException;
+import com.teampingui.models.JournalEntryItem;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ExampleTest {
+public class ExampleTest extends Database {
 
     @Test
-    public void twoMultipliedByTwoIsFour() {
-        assertEquals(4, 2 * 2, "2 x 2 must be 4");
+    public void testInsertJournalEntry() {
+        // This test might delete all existing data in the database
+
+        Database.connect();
+        JournalDAO journalDAO = new JournalDAO();
+
+        // A loop to delete every item from the journal database
+        JournalEntryItem journalEntry = new JournalEntryItem("01.01.2020", "This text came from Unit Testing");
+        try {
+            journalDAO.insert(journalEntry);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (JournalDaoException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(1, journalDAO.getAll().size());
+
+        // Remove last entry from database
+        journalDAO.delete(journalEntry);
     }
 }
