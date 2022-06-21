c00f1479da8de60eeac52e1058f89e24a6ddc866
jb266 <jb266@hdm-stuttgart.de>
2022-06-21 11:41:22 +0200

    Add error messages to Habitdialog (input required and 1 least 1 day required)

--- src/main/java/com/teampingui/controllers/AddHabitDialogController.java
+++ src/main/java/com/teampingui/controllers/AddHabitDialogController.java
@@ -2,15 +2,22 @@ package com.teampingui.controllers;
 
 import com.teampingui.dao.HabitDAO;
 import com.teampingui.models.Habit;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,9 +26,21 @@ public class AddHabitDialogController implements Initializable {
     @FXML
     CheckBox cbMonday, cbTuesday, cbWednesday, cbThursday, cbFriday, cbSaturday, cbSunday; // TODO: instead of adding each checkbox, just add the container and access checkboxes through .getItems()
     @FXML
-    Label lAddHabitHeading;
+    Label lAddHabitHeading, lErrorMsg;
     @FXML
     TextField tfNewHabitName;
+    @FXML
+    VBox vbErrorContainer;
+    @FXML
+    ProgressBar pbErrorDuration;
+    private static final Integer ERROR_DIALOG_TIME = 3;
+    private Timeline mTimeline;
+    private IntegerProperty mDialogTime = new SimpleIntegerProperty(ERROR_DIALOG_TIME * 100);
+
+    private Thread mThreadErrorMsg;
+
+    private static final Logger log = LogManager.getLogger(MainController.class);
+
 
     private HabitDAO mHabitDAO;
 
@@ -36,9 +55,14 @@ public class AddHabitDialogController implements Initializable {
     void addNewHabit(ActionEvent e) {
         String name = tfNewHabitName.getText().trim();
         if (name.length() > 15 || name.length() <= 0) {
-            // TODO: Add Error Message?
+            showError("Inputfield can not be empty!");// TODO: Add Error Message?
             return;
-        } else {
+        }
+        else if(!(cbMonday.isSelected()||cbTuesday.isSelected()||cbWednesday.isSelected()||cbThursday.isSelected()||cbFriday.isSelected()||cbSaturday.isSelected()||cbSunday.isSelected())){
+            showError("You have to select at least 1 day");
+            tfNewHabitName.clear();
+            return;
+        }else {
             name = tfNewHabitName.getText().trim();
             tfNewHabitName.clear();
         }
@@ -58,12 +82,47 @@ public class AddHabitDialogController implements Initializable {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
         closeStage(e);
     }
 
     public void setHabitDAO(HabitDAO habitDAO) {
         this.mHabitDAO = habitDAO;
+    }
+
+    public void showError(String msg) {
+        vbErrorContainer.setVisible(true);
+        lErrorMsg.setText(msg);
+
+        if (mTimeline != null) {
+            mTimeline.stop();
+        }
+
+        mDialogTime.set(ERROR_DIALOG_TIME * 100);
+        mTimeline = new Timeline();
+        mTimeline.getKeyFrames().add(
+                new KeyFrame(Duration.seconds(ERROR_DIALOG_TIME),
+                        new KeyValue(mDialogTime, 0))
+        );
+        mTimeline.playFromStart();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(ERROR_DIALOG_TIME * 1000L);
+                    Platform.runLater(() -> vbErrorContainer.setVisible(false));
+                } catch (InterruptedException e) {
+                    log.debug("Interrupted");
+                }
+            }
+        };
+
+        if (mThreadErrorMsg != null && mThreadErrorMsg.isAlive()) {
+            mThreadErrorMsg.interrupt();
+        }
+
+        mThreadErrorMsg = new Thread(runnable);
+        mThreadErrorMsg.start();
     }
 
     public void closeStage(ActionEvent e) {
