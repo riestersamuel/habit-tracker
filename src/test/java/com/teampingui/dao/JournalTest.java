d915e06f584346badf6ab299450dcb95d0a53ad4
riestersamuel <riestersamuel@icloud.com>
2022-06-21 10:30:17 +0200

    Reworked JournalTest.java

--- src/test/java/com/teampingui/dao/JournalTest.java
+++ src/test/java/com/teampingui/dao/JournalTest.java
@@ -2,17 +2,22 @@ package com.teampingui.dao;
 
 import com.teampingui.exceptions.JournalDaoException;
 import com.teampingui.models.JournalEntryItem;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.sql.SQLException;
 
 class JournalTest {
+    JournalDAO journalDAO = new JournalDAO();
+    JournalEntryItem journalEntry = new JournalEntryItem("01.01.2020", "This text came from JournalTest.java");
+
+    @BeforeEach
+    public void setUp() {
+        Database.connect();
+    }
+
     @Test
     public void testInsertJournalEntry() {
         //TODO: Check if that's correct testing method
-        Database.connect();
-        JournalDAO journalDAO = new JournalDAO();
-        JournalEntryItem journalEntry = new JournalEntryItem("01.01.2020", "This text came from Unit Testing");
         try {
             journalDAO.insert(journalEntry);
         } catch (SQLException e) {
@@ -20,9 +25,14 @@ class JournalTest {
         } catch (JournalDaoException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @AfterEach
+    public void tearDown() {
 
         // Does the list of entries contain the new entry?
         //Assumptions.assumeTrue(journalDAO.mosJournalEntries.contains(journalEntry)); // TODO: Dont change access modifier because of unit test!
+        Assumptions.assumeTrue(journalDAO.getAll().contains(journalEntry));
 
         // Remove last entry from list and database
         //TODO: After implemeting the delete method, check or manage that the new entry is also removed from the database
