dab462d249fb068b8ff6a06efcc10ac7b758725e
riestersamuel <riestersamuel@icloud.com>
2022-06-21 16:59:09 +0200

    Fully implemented the negative journal test

--- src/test/java/com/teampingui/dao/NegativeJournalTest.java
+++ src/test/java/com/teampingui/dao/NegativeJournalTest.java
@@ -2,15 +2,23 @@ package com.teampingui.dao;
 
 import com.teampingui.exceptions.JournalDaoException;
 import com.teampingui.models.JournalEntryItem;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 
 import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assume.assumeThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NegativeJournalTest {
     JournalDAO journalDAO = new JournalDAO();
     JournalEntryItem journalEntry = new JournalEntryItem("05.05.1999", null);
 
-    @Test
+    //We assume that a runtime exception is thrown when the entry is null
+    @Test (expected = RuntimeException.class)
     public void testInsertJournalEntry() {
         try {
             journalDAO.insert(journalEntry);
@@ -19,6 +27,5 @@ public class NegativeJournalTest {
         } catch (JournalDaoException e) {
             throw new RuntimeException(e);
         }
-        //TODO: Update database: Add NOT NULL statement to entry column
     }
 }
