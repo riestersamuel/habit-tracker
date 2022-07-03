7427a4d1fcd4491ec4729463428e301c74404d37
jb266 <jb266@hdm-stuttgart.de>
2022-07-03 11:02:58 +0200

    removed date format from settings

--- src/main/java/com/teampingui/models/Settings.java
+++ src/main/java/com/teampingui/models/Settings.java
@@ -7,7 +7,6 @@ public class Settings { // TODO: static... singleton..., ...?
     private static final Preferences prefs;
 
     private static final String USERNAME = "username";
-    private static final String DATE_FORMAT = "date_format";
 
     static {
         prefs = Preferences.userNodeForPackage(Settings.class);
@@ -20,13 +19,4 @@ public class Settings { // TODO: static... singleton..., ...?
     public static void setUsername(String username) {
         prefs.put(USERNAME, username);
     }
-
-    public static String getDateFormat() {
-        return prefs.get(DATE_FORMAT, "dd.mm.yyyy");
-    }
-
-    public static void setDateFormat(String dateFormat) {
-        prefs.put(DATE_FORMAT, dateFormat);
-    }
-
 }
