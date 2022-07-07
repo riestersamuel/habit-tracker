dae066dfe79af3ef4cf45f9acc692ee18df1d770
jb266 <jb266@hdm-stuttgart.de>
2022-07-07 10:54:57 +0200

    removed unused imports

--- src/main/java/com/teampingui/models/ButtonCell.java
+++ src/main/java/com/teampingui/models/ButtonCell.java
@@ -1,8 +1,6 @@
 package com.teampingui.models;
 
 import com.teampingui.interfaces.IButtonClickListener;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 
